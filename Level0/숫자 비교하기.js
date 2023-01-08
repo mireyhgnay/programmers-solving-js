81a94c18f38b9803fe348a4a8dd08216074423b3
hyerim <hyerimiya1216@gmail.com>
2023-01-08 20:02:38 +0900

    [solving] 숫자 비교하기 풀이

---
+++ Level0/숫자 비교하기.js
@@ -0,0 +1,14 @@
+// my answer
+function solution(num1, num2) {
+    if(num1 === num2) {
+        return 1;
+    }else {
+        return -1;
+    }
+}
+
+// solution
+function solution(num1, num2) {
+    const answer = num1 === num2 ? 1 : -1;
+    return answer;
+}
