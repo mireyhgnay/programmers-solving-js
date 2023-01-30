51d399f180f85e515dafd554e6629955b3d38d86
hyerim <hyerimiya1216@gmail.com>
2023-01-31 00:37:47 +0900

    Solve : 문자열 계산하기 풀이

---
+++ Level0/문자열 계산하기.js
@@ -0,0 +1,9 @@
+const solution = my_string => eval(my_string);
+
+// other solution - 심플..그자체..
+let solution = eval;
+
+/*
+eval()은 문자로 표현된 JavaScript 코드를 실행하는 함수입니다.
+주의할 점은 해커가 위험한 코드를 사용할 수 있어 사용에 주의해야한다.
+*/
