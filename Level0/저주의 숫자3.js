1136e7a2334567ac6fd2bfce77d429400a0d4c9b
hyerim <hyerimiya1216@gmail.com>
2023-02-01 22:57:11 +0900

    Solve : 저주의숫자3 풀이

---
+++ Level0/저주의 숫자3.js
@@ -0,0 +1,7 @@
+// best solution
+function solution(n) {
+    let arr = [];
+    let num = 0;
+    while (arr.length !== n && ++num) if (num%3!==0 && !(''+num).includes('3')) arr.push(num);
+    return arr.pop();
+}
