2a4873c7186a99a42198f39be41dd950777ff2f9
hyerim <hyerimiya1216@gmail.com>
2023-02-10 22:49:46 +0900

    Solve : 나누어 떨어지는 숫자 배열 풀이

---
+++ Level1/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,20 @@
+function solution(arr, divisor) {
+    let answer = [];
+    
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i] % divisor == 0) {
+        answer.push(arr[i]);
+      } else if(answer.length == 0) {
+          answer.push(-1)
+      }
+    }
+    
+    answer.sort((a, b) => a - b);
+    return answer;
+}
+
+// other solution - filter
+function solution(arr, divisor) {
+    var answer = arr.filter(v => v % divisor === 0);
+    return answer.length == 0 ? [-1] : answer.sort((a, b) => a - b);
+}
