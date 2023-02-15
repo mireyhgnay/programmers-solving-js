78dc392e7a84c0b505caa9b026880e1250d67989
hyerim <hyerimiya1216@gmail.com>
2023-02-15 14:49:07 +0900

    Solve : 행렬의 덧셈 풀이

---
+++ Level1/행렬의 덧셈.js
@@ -0,0 +1,18 @@
+function solution(arr1, arr2) {
+    let answer = [];
+    
+    for(let i = 0; i < arr1.length; i++){  
+        let sum = [];
+        for(let j=0; j < arr1[i].length; j++){ 
+            sum.push(arr1[i][j] + arr2[i][j]);
+        }
+        answer.push(sum);
+    }
+    return answer;
+}
+
+
+// best solution - map 
+function solution(arr1, arr2) {
+    return arr1.map((a,i) => a.map((b, j) => b + arr2[i][j]));
+}
