780ec8dc66fb3541928129c3c7892dee05435b4f
hyerim <hyerimiya1216@gmail.com>
2023-02-21 23:05:02 +0900

    Solve : 숫자 문자열과 영단어 풀이

---
+++ Level1/숫자 문자열과 영단어.js
@@ -0,0 +1,31 @@
+function solution(s) {
+    let answer = 0;
+    
+		// replace, 정규표현식
+    s = s.replace(/zero/g, 0);
+    s = s.replace(/one/g, 1);
+    s = s.replace(/two/g, 2);
+    s = s.replace(/three/g, 3);
+    s = s.replace(/four/g, 4);
+    s = s.replace(/five/g, 5);
+    s = s.replace(/six/g, 6);
+    s = s.replace(/seven/g, 7);
+    s = s.replace(/eight/g, 8);
+    s = s.replace(/nine/g, 9);
+    
+    answer = Number(s);
+    return answer;
+}
+
+// best solution
+function solution(s) {
+    const numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let answer = s;
+
+    for(let i = 0; i< numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i);
+    }
+
+    return Number(answer);
+}
