539a8243a29820aa7f1477e56b7a69092073c3aa
hyerim <hyerimiya1216@gmail.com>
2023-02-23 22:09:04 +0900

    Solve : 최솟값 구하기 Math 메서드 추가 공부

--- Section1/최솟값 구하기.js
+++ Section1/최솟값 구하기.js
@@ -11,3 +11,11 @@ function solution(arr){
 
 let arr=[5, 7, 1, 3, 2, 9, 11];
 console.log(solution(arr)); // 1
+
+
+// 배열 원소 중 최솟값,최댓값을 구해주는 Math 메서드 사용하기
+function solution(arr){
+    return Math.min(...arr);
+    // 배열을 인자로 넣어줄때 그냥 arr 변수명으로 넣어주면 안되고 전개연산자(...)로 적어줘야함
+    // arr[0], arr[1], arr[2] 이런식으로 넣어준 것
+}
