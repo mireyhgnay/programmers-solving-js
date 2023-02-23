8d05d7745c7a61bee0ddf1e3216ea51de932c5ba
hyerim <hyerimiya1216@gmail.com>
2023-02-23 22:14:42 +0900

    Solve : 홀수 풀이

---
+++ Section1/홀수.js
@@ -0,0 +1,18 @@
+function solution(arr){
+    let answer = [];
+    let sum = 0;
+    let min = 1000;
+
+    for(let x of arr){
+        if(x%2 === 1){ // 나머지가 1일떄 (=홀수)
+            sum += x; // 홀수의 합들을 sum에 저장 (sum = sum + x)
+            if(x < min) min = x;
+        }
+    }
+    answer.push(sum);
+    answer.push(min);     
+    return answer;
+}
+
+// arr=[12, 77, 38, 41, 53, 92, 85];
+// console.log(solution(arr));
