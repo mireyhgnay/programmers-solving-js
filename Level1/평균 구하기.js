43f678ea22873521bc2f691dc19003c183727172
hyerim <hyerimiya1216@gmail.com>
2023-04-17 22:00:05 +0900

    Solve : [Programmers][LV1] 평균 구하기

--- Level1/평균 구하기.js
+++ Level1/평균 구하기.js
@@ -1,1 +1,7 @@
-const solution = arr => arr.reduce((a, b) => a + b, 0) / arr.length;
+const solution = (arr) => arr.reduce((a, b) => a + b) / arr.length;
+
+/*
+MDN : Array.prototype.reduce()
+  https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+  - 배열의 각 요소에 대해 주어진 리듀서 (reducer) 함수를 실행하고, 하나의 결과값을 반환한다.
+*/
