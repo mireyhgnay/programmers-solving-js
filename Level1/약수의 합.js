8950dae74a5496778f107cad5fb6df5976dd8eed
hyerim <hyerimiya1216@gmail.com>
2023-04-18 20:57:51 +0900

    Solve : [LV1] 약수의 합

--- Level1/약수의 합.js
+++ Level1/약수의 합.js
@@ -1,12 +1,20 @@
-function solution(n) {
-    let array = [];
-
-    for (let i = 0; i <= n; i++) {
-        if (n % i === 0) { // 나눴을 때 나머지가 0인 경우(i가 n의 약수인 경우)
-            array.push(i); // 배열에 추가
-        }
+const solution = (n) => {
+  // 약수 : 나눴을 때 0이 되는 수
+  let divisorArray = [];
+  for (let i = 0; i <= n; i++) {
+    if (n % i === 0) {
+      divisorArray.push(i);
     }
-    
-    // 배열의 원소 더하기
-    return array.reduce((a, b) => a + b, 0);
+  }
+  return divisorArray.reduce((a, b) => a + b);
+};
+
+// best solution
+// 굳이 배열로 만들어서 push 하여 요소들을 더할 필요 없이 바로 약수인 i 들만 더해줘서 sum을 반환하기
+function solution(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) sum += i;
+  }
+  return sum;
 }
