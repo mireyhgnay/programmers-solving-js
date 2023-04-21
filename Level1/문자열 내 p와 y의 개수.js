c0dd2882a15403f6775c80fdabfefcaab09776e3
hyerim <hyerimiya1216@gmail.com>
2023-04-21 23:08:16 +0900

    Solve : [LV1] 문자열 내 p와 y의 개수

--- Level1/문자열 내 p와 y의 개수.js
+++ Level1/문자열 내 p와 y의 개수.js
@@ -1,8 +1,13 @@
+const solution = (word) => {
+  // split('P') : 배열에서 P 만 골라내기
+  // 'pPoooyY' 면, ['', '', 'OOOYY']
+  const pString = word.toUpperCase().split("P");
+  const yString = word.toUpperCase().split("Y");
+
+  return pString.length === yString.length ? true : false;
+};
+
+// other solution : 정규식 활용
 function solution(s) {
-    // toUpperCase() : 대문자로 변환
-    // split('P') : 배열에서 P 만 골라내기
-    const p = s.toUpperCase().split('P');
-    const y = s.toUpperCase().split('Y');
-    
-    return p.length === y.length ? true : false;
+  return s.match(/p/gi).length == s.match(/y/gi).length;
 }
