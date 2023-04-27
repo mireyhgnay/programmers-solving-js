e0ba058f1c91fff19dffc3d33ca1d269fa6fbda2
hyerim <hyerimiya1216@gmail.com>
2023-04-27 22:38:07 +0900

    Solve : [LV1] 정수 내림차순으로 배치하기

--- Level1/정수 내림차순으로 배치하기.js
+++ Level1/정수 내림차순으로 배치하기.js
@@ -1,9 +1,18 @@
-function solution(n) {
-    return Number(n.toString().split('').sort((a, b) => b - a).join(''));
-}
+const solution = (n) => {
+  // 문자열로 변환 -> 배열로 변환 -> 내림차순 정렬 -> 문자열로 변환
+  return Number(
+    n
+      .toString()
+      .split("")
+      .sort((a, b) => b - a)
+      .join("")
+  );
+};
 
-// toString() : 문자열로 변환 (문자열이여야 배열로 변환할 수 있음)
-// split('') : 배열로 변환
-// sort() : 배열 정렬하기 (오름차순(a-b), 내림차순(b-a), ..)
-// join('') : 배열 문자열 합치기
-// Number() : 숫자로 변환
+// other solution
+const solution = (n) => {
+  return parseInt((n + "").split("").sort().reverse().join(""));
+};
+// 문자열을 숫자로 변환 : Number() -> parseInt() 사용
+// 숫자를 문자열로 변환 : toString() -> n + '' 사용
+// 내림차순 : sort 연산 -> sort().reverse() 배열의 순서를 반전시킨다
