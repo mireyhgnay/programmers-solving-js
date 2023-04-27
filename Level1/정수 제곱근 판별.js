eff14277b4770b680a4b8bf9cd085574d2823808
hyerim <hyerimiya1216@gmail.com>
2023-04-27 22:35:09 +0900

    Solve : [LV1] 정수 제곱근 판별

--- Level1/정수 제곱근 판별.js
+++ Level1/정수 제곱근 판별.js
@@ -1,16 +1,6 @@
-function solution(n) {
-    // 제곱근 판별하기
-    const sqrt = Math.sqrt(n);
-    
-    if ( sqrt % 1 == 0 ) { // 제곱근일 경우
-        return answer = Math.pow(sqrt + 1, 2);
-        // 숫자가 2라면, 2번 곱하고(2 * 2 = 4), 1 더하기(5)
-        // Math.pow(base, exponent): base 에 exponent를 제곱한 값을 반환
-    }
-    
-    return -1;
-}
-
-
-// other solution - 삼항 연산자를 이용한 풀이
-const solution = n => Math.sqrt(n) % 1 == 0 ? Math.pow(Math.sqrt(n) + 1, 2) : -1;
+const solution = (n) => {
+  // n의 제곱근 구하기
+  const sqrt = Math.sqrt(n);
+  // 양의 정수일 경우에만 더하기 1, 거듭제곱 값을 출력한다
+  return sqrt % 1 == 0 ? Math.pow(sqrt + 1, 2) : -1;
+};
