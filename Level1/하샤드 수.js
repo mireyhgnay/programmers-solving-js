40dda4dff9cfd30ee54c878a9f4f6f7256fac23a
hyerim <hyerimiya1216@gmail.com>
2023-05-15 18:57:49 +0900

    Solve : [LV1] 하샤드 수

--- Level1/하샤드 수.js
+++ Level1/하샤드 수.js
@@ -1,9 +1,22 @@
 function solution(x) {
-    let num = 0;
-    const arr = x.toString().split('');
-    for (let i = 0; i < arr.length; i++) {
-        num += Number(arr[i]); // 배열 원소끼리 더하기
-    }
-    
-    return x % num === 0 ? true : false;
+  let num = 0;
+  const arr = x.toString().split("");
+  for (let i = 0; i < arr.length; i++) {
+    num += Number(arr[i]); // 배열 원소끼리 더하기
+  }
+
+  return x % num === 0 ? true : false;
+  // x 가 10이면, 10 % 1 나머지가 0이니까 true
+}
+
+// other solution
+function solution(x) {
+  let num = x;
+  let sum = 0;
+  do {
+    sum += x % 10;
+    x = Math.floor(x / 10);
+  } while (x > 0);
+
+  return !(num % sum);
 }
